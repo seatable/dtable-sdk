1616a2cfa0dfdd8e48482e43a9827525e24daeec
Michael An <2331806369@qq.com>
2019-12-28 11:39:29 +0800

    update codes (#9)

--- example/example.js
+++ example/example.js
@@ -9,7 +9,7 @@ const config = JSON.parse(fs.readFileSync(configPath).toString());
 const { APIToken, server, workspaceID, dtableName } = config;
 
 let dtableConfig = {
-  server: server.replace(/\/+$/, ""),
+  server: server.replace(/\/+$/, ''),
   APIToken,
   workspaceID,
   dtableName,
@@ -19,11 +19,12 @@ class Test {
   constructor(dtable) {
     this.dtable = dtable;
   }
+
   sayHello() {
     let { dtable } = this;
     let table = dtable.getTableByName('sayHello');
     if (!table) return;
-    dtable.forEachRow('sayHello', 'Default_View', (row) => { 
+    dtable.forEachRow('sayHello', 'Default_View', (row) => {
       let name = row['Name'];
       const res = `Hello ${name}`;
       if (row['Result'] && row['Result'] === res) {
