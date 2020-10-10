f26a6f223ad7b9d5a56ad5084839ac3dd64704b4
cainiao222 <1325442034@qq.com>
2020-10-10 16:38:58 +0800

    fix code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Dtable from './dtable';
-import { COLUMNS_ICON_CONFIG, CellType as CELLTYPE } from 'dtable-store';
+import { COLUMNS_ICON_CONFIG, CellType as CELL_TYPE } from 'dtable-store';
 
 export default Dtable;
-export { COLUMNS_ICON_CONFIG, CELLTYPE };
+export { COLUMNS_ICON_CONFIG, CELL_TYPE };
