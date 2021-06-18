792968ef25cc6d17337353f8223e4bde079f21d4
cainiao222 <1325442034@qq.com>
2021-06-18 10:34:01 +0800

    export dtable-store column-options

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Dtable from './dtable';
-import { COLUMNS_ICON_CONFIG, CellType as CELL_TYPE } from 'dtable-store';
+import { COLUMNS_ICON_CONFIG, CellType as CELL_TYPE, COLUMN_OPTIONS } from 'dtable-store';
 import StatUtils from './stat-utils';
 
 export default Dtable;
-export { COLUMNS_ICON_CONFIG, CELL_TYPE, StatUtils };
+export { COLUMNS_ICON_CONFIG, CELL_TYPE, StatUtils, COLUMN_OPTIONS };
