95cdfd6e6be64bdba96023d185c3ef99469f8175
cainiao222 <1325442034@qq.com>
2021-07-17 15:34:31 +0800

    export dtable-store formatDurationToNumber

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Dtable from './dtable';
-import { COLUMNS_ICON_CONFIG, CellType as CELL_TYPE, COLUMN_OPTIONS } from 'dtable-store';
+import { COLUMNS_ICON_CONFIG, CellType as CELL_TYPE,
+  COLUMN_OPTIONS,
+  formatDurationToNumber
+} from 'dtable-store';
 import StatUtils from './stat-utils';
 
 export default Dtable;
-export { COLUMNS_ICON_CONFIG, CELL_TYPE, StatUtils, COLUMN_OPTIONS };
+export { COLUMNS_ICON_CONFIG, CELL_TYPE, StatUtils, COLUMN_OPTIONS, formatDurationToNumber };
