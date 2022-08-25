b0a8fb62acd818e1f569f46c4d1debbef81bdfa5
renjie-run <rj.aiyayao@gmail.com>
2022-08-26 00:02:24 +0800

    refactor: update statistic utils

--- src/stat-utils.js
+++ src/stat-utils.js
@@ -1,12 +1,9 @@
 import {
   Views, 
-  TableUtils,
   DateUtils,
-  RowUtils,
   isNumber, 
   CellType,
   FORMULA_RESULT_TYPE,
-  FORMULA_COLUMN_TYPES,
   sortText,
   sortNumber,
   sortDate,
@@ -17,25 +14,7 @@ import {
   getDateDisplayString,
   getNumberDisplayString,
   getCellValueDisplayString,
-  getLinkDisplayString,
 } from 'dtable-store';
-
-const STATISTICS_COUNT_TYPE = {
-  COUNT: 'count',
-  ADVANCED: 'advanced',
-  DAY: 'day',
-  WEEK: 'week',
-  QUARTAR: 'quartar',
-  MONTH: 'month',
-  YEAR: 'year',
-  SUM: 'Sum',
-  MAX: 'Max',
-  MIN: 'Min',
-  MEAN: 'Mean',
-  PROVINCE: 'province',
-  CITY: 'city',
-  DISTRICT: 'district'
-};
 
 const SUPPORT_SORT_COLUMNS = [
   CellType.TEXT, 
@@ -55,29 +34,6 @@ class StatUtils {
     return Views.getViewRows(view, table, value, username, userId);
   }
 
-  static isValidRow(row, formulaRow, linkRow, column, includeEmpty) {
-    const { type: columnType, key: columnKey } = column;
-    if (includeEmpty || columnType === CellType.CHECKBOX) return true;
-    let cellValue;
-    if (columnType === CellType.FORMULA || columnType === CellType.LINK_FORMULA) {
-      cellValue = formulaRow ? formulaRow[columnKey] : null;
-    } else if (columnType === CellType.GEOLOCATION) {
-      const value = row[columnKey];
-      const { geo_format } = column.data;
-      return isEmptyGeolocationCell(value, geo_format);
-    } else if (columnType === CellType.LINK) {
-      cellValue = linkRow ? linkRow[columnKey] : null;
-    } else {
-      cellValue = row[columnKey];
-    }
-
-    return cellValue || cellValue === 0;
-  }
-
-  static getLinkDisplayString(rowIds, linkedTable, displayColumnKey = '0000') {
-    return getLinkDisplayString(rowIds, linkedTable, displayColumnKey);
-  }
-
   static getNumberDisplayString(value, columnData) {
     return getNumberDisplayString(value, columnData);
   }
@@ -86,18 +42,17 @@ class StatUtils {
     return Views.getTableFormulaResults(table, rows, value);
   }
 
-  static getTableLinkRows(rows, table, value) {
-    return RowUtils.getTableLinkRows(rows, table, value);
-  }
-
   static getCellValueDisplayString(row, type, key, {tables = [], formulaRows = {}, data, collaborators = []}) {
     return getCellValueDisplayString(row, type, key, {tables, formulaRows, data, collaborators});
+  }
+
+  static getFormulaDisplayString(cellValue, columnData) {
+    return getFormulaDisplayString(cellValue, columnData);
   }
 
   static getGroupLabel(
     cellValue,
     formulaRow,
-    linkRow,
     column,
     dateGranularity,
     geoGranularity,
@@ -167,9 +122,11 @@ class StatUtils {
         return cellValue ? cellValue[geoGranularity] : null;
       }
       case CellType.LINK: {
-        if (!linkRow) return null;
-        let linkCellValue = linkRow[key] || cellValue;
-        return linkCellValue || [];
+        const linkCellValue = formulaRow && formulaRow[key];
+        if (!Array.isArray(linkCellValue)) {
+          return [];
+        }
+        return linkCellValue.map(linkVal => linkVal.display_value);
       }
       case CellType.CHECKBOX: {
         return String(!!cellValue);
@@ -182,119 +139,6 @@ class StatUtils {
         return null;
       }
     }
-  }
-
-  static getTotal(
-    summary_column_key,
-    summary_column_type,
-    summary_type,
-    summary_method,
-    rows = [],
-    formula_rows = {}
-  ) {
-    let rowsLength = rows.length;
-    let total;
-    if (summary_type === STATISTICS_COUNT_TYPE.COUNT) {
-      total = rowsLength;
-    } else if (summary_type === STATISTICS_COUNT_TYPE.ADVANCED) {
-      switch (summary_method) {
-        case 'Distinct_values': {
-          let count = 0;
-          let existMap = {};
-          rows.forEach(r => {
-            let num;
-            if (FORMULA_COLUMN_TYPES.includes(summary_column_type)) {
-              const formulaRow = formula_rows[r._id] || {};
-              num = formulaRow[summary_column_key];
-              num = Array.isArray(num) ? num[0] : num;
-            } else {
-              num = r[summary_column_key];
-            }
-            if (!num) {
-              if (num === 0) {
-                num = '0';
-              } else {
-                num = !!num;
-              }
-            }
-            if (!existMap[num]) {
-              existMap[num] = true;
-              count++;
-            }
-          });
-          total = count;
-          break;
-        }
-        case "Sum":
-        case "Mean": {
-          let sum = 0;
-          let validNumbersCount = 0;
-          rows.forEach(r => {
-            let num;
-            if (FORMULA_COLUMN_TYPES.includes(summary_column_type)) {
-              let formulaRow = formula_rows[r._id] || {};
-              num = formulaRow[summary_column_key];
-              num = Array.isArray(num) ? num[0] : num;
-            } else {
-              num = r[summary_column_key];
-            }
-            if (isNumber(num)) {
-              validNumbersCount++;
-              sum += num;
-            }
-          });
-          if (summary_method === "Sum") {
-            total = Number.parseFloat(sum.toFixed(8));
-          } else if (summary_method === "Mean") {
-            total =
-              validNumbersCount === 0
-                ? 0
-                : Number.parseFloat((sum / validNumbersCount).toFixed(8));
-          }
-          break;
-        }
-        case "Max":
-        case "Min": {
-          if (rowsLength > 0) {
-            let result = rows.reduce((current, next) => {
-              let currentValue, nextValue;
-              if (FORMULA_COLUMN_TYPES.includes(summary_column_type)) {
-                let currentFormulaRow = formula_rows[current._id];
-                let nextFormulaRow = formula_rows[next._id];
-                currentValue = currentFormulaRow && currentFormulaRow[summary_column_key];
-                nextValue = nextFormulaRow && nextFormulaRow[summary_column_key];
-                currentValue = Array.isArray(currentValue) ? currentValue[0] : currentValue;
-                nextValue = Array.isArray(nextValue) ? nextValue[0] : nextValue;
-              } else {
-                currentValue = current[summary_column_key];
-                nextValue = next[summary_column_key];
-              }
-              if (!nextValue && nextValue !== 0) {
-                return current;
-              }
-              let isNextGreater = currentValue < nextValue;
-              if (summary_method === "Min") {
-                return isNextGreater ? current : next;
-              } else {
-                return isNextGreater ? next : current;
-              }
-            });
-            if (FORMULA_COLUMN_TYPES.includes(summary_column_type)) {
-              let formulaRow = formula_rows[result._id];
-              total = formulaRow && formulaRow[summary_column_key];
-              total = Array.isArray(total) ? total[0] : total;
-            } else {
-              total = result[summary_column_key];
-            }
-          }
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    }
-    return total || 0;
   }
 
   static sortStatistics(statistics, column, sort_key) {
@@ -358,31 +202,6 @@ class StatUtils {
     });
   }
 
-  static sortStatisticData(statistics, sort_type) {
-    statistics.sort((currResult, nextResult) => {
-      let { value: current } = currResult;
-      let { value: next } = nextResult;
-      if (!current && current !== 0) {
-        return -1;
-      }
-      if (!next && next !== 0) {
-        return 1;
-      }
-      if (sort_type === 'ascending') {
-        return current > next ? 1 : -1;
-      } else {
-        return current > next ? -1 : 1;
-      }
-    });
-  };
-
-  static isArrayCellValue(columnType) {
-    return [
-      CellType.MULTIPLE_SELECT,
-      CellType.COLLABORATOR,
-      CellType.LINK
-    ].includes(columnType);
-  }
 }
 
 const getValidCollaborators = (collaborators, emails) => {
@@ -391,16 +210,5 @@ const getValidCollaborators = (collaborators, emails) => {
   }
   return emails.filter(e => collaborators.findIndex(c => c.email === e) > -1);
 };
-
-const isEmptyGeolocationCell = (value, format) => {
-  if (!value) return null;
-  if (format === 'lng_lat') {
-    return value.lng && value.lat;
-  }
-  if (format === 'country_region') {
-    return value.country_region
-  }
-  return value.province;
-}
 
 export default StatUtils;
