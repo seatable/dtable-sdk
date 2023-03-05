57575ed349c32e2cc0da29329222038b54dcd8ac
renjie-run <rj.aiyayao@gmail.com>
2023-03-05 20:24:33 +0800

    feat: add api

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import Dtable from './dtable';
-import { 
-  CellType as CELL_TYPE,
-  COLUMN_OPTIONS,
-  COLUMNS_ICON_CONFIG, 
-  FORMULA_RESULT_TYPE,
-  SELECT_OPTION_COLORS,
-  TABLE_PERMISSION_TYPE,
-  formatDurationToNumber,
-  sortDate,
+import {
+  CellType as CELL_TYPE, COLUMN_OPTIONS, COLUMNS_ICON_CONFIG, DEFAULT_NUMBER_FORMAT,
+  FILTER_COLUMN_OPTIONS, FILTER_PREDICATE_TYPE, FORMULA_RESULT_TYPE, SELECT_OPTION_COLORS,
+  TABLE_PERMISSION_TYPE, formatDurationToNumber, formatStringToNumber, filterRow,
+  getValidFilters, getNumberDisplayString, replaceNumberNotAllowInput, sortDate,
 } from 'dtable-store';
 import StatUtils from './stat-utils';
 
 export default Dtable;
-export { 
-  CELL_TYPE, 
-  COLUMN_OPTIONS, 
-  COLUMNS_ICON_CONFIG, 
-  FORMULA_RESULT_TYPE,
-  SELECT_OPTION_COLORS,
-  TABLE_PERMISSION_TYPE, 
-  formatDurationToNumber,
-  StatUtils, 
-  sortDate,
+export {
+  CELL_TYPE, COLUMN_OPTIONS, COLUMNS_ICON_CONFIG, DEFAULT_NUMBER_FORMAT, FILTER_COLUMN_OPTIONS,
+  FILTER_PREDICATE_TYPE, FORMULA_RESULT_TYPE, SELECT_OPTION_COLORS, TABLE_PERMISSION_TYPE,
+  formatDurationToNumber, formatStringToNumber, filterRow, getValidFilters, getNumberDisplayString,
+  replaceNumberNotAllowInput, sortDate,
+  StatUtils,
 };
